93c8aa8667766c507557f932c3edb2a5cecbee0a
emeri-e <124933489+emeri-e@users.noreply.github.com>
2025-02-18 11:04:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,12 @@ async def hello_world_endpoint(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    print("Starting webserver...")
+    uvicorn.run(
+        api,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        debug=os.getenv("DEBUG", False),
+        log_level=os.getenv('LOG_LEVEL', "info"),
+        proxy_headers=True
+    )
